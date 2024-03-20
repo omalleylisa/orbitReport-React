54777b09b68c7960d3ca6614b6b55785d882ad2f
Lisa O'Malley <lisaomalley@Lisas-MBP.lan>
2024-03-19 19:36:01 -0500

    GA 6, first commit attempt

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,19 @@
+import satData from "./satData";
+import './styling.css';
+
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+      {displaySats.map((sat,id) => {
+        return (
+          <button onClick={() => filterByType(sat)} key={id}
+          >
+            {sat} Orbit
+          </button>
+        );
+      })}
+      <button onClick={() => setSat(satData)} key="all-orbits">All Orbits</button>
     </div>
   );
 };
